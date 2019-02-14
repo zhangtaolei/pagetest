c8bf34b8a69e53b8998fd6209794a16a08d28beb
zhangtao <zhangtao3@lizihang.com>
2019-02-14 10:21:24 +0800

    modify

--- sw.js
+++ sw.js
@@ -1,42 +1,27 @@
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.1.0/workbox-sw.js");
-var cacheStorageKey = 'minimal-pwa-2'
-var cacheList=[
-  '/',
-  'index.html',
-  'main.css',
-  'youhun.jpg'
-]
-self.addEventListener('install',e =>{
-  e.waitUntil(
-    caches.open(cacheStorageKey)
-    .then(cache => cache.addAll(cacheList))
-    .then(() => self.skipWaiting())
-  )
-})
-self.addEventListener('fetch',function(e){
-  e.respondWith(
-    caches.match(e.request).then(function(response){
-      if(response != null){
-        return response
-      }
-      return fetch(e.request.url)
-    })
-  )
-})
-self.addEventListener('activate',function(e){
-  e.waitUntil(
-    //获取所有cache名称
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        // 获取所有不同于当前版本名称cache下的内容
-        cacheNames.filter(cacheNames => {
-          return cacheNames !== cacheStorageKey
-        }).map(cacheNames => {
-          return caches.delete(cacheNames)
-        })
-      )
-    }).then(() => {
-      return self.clients.claim()
-    })
-  )
-})
+var cacheName = 'hello-pwa';
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(cacheName)
+          .then(cache => cache.addAll(
+            [
+                '/',  // 这个一定要包含整个目录，不然无法离线浏览
+                './youhun.jpg',
+                './index.html',
+                './main.css'
+            ]
+          )).then(() => self.skipWaiting())
+    );
+});
+
+self.addEventListener('fetch', function (event) {  
+    event.respondWith(
+      caches.match(event.request)                    
+      .then(function (response) {
+        if (response) {                              
+          return response;                           
+        }
+        return fetch(event.request);                 
+      })
+    );
+});
